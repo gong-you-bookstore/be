4017f95ff8a7d301c5a3920e42ab07eca6421219
zogak <zogak7@gmail.com>
2022-10-18 18:26:54 +0900

    init

---
+++ sharedBook/src/main/java/com/bookstore/sharedBook/SharedBookApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.sharedBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedBookApplication.class, args);
+	}
+
+}
---
+++ sharedBook/src/test/java/com/bookstore/sharedBook/SharedBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.sharedBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
